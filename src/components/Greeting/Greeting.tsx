4bd0b0480dfa477d81216a01a2268f8f000d6d23
Max <linme00p@gmail.com>
2022-06-13 20:02:14 +0200

    Create designed Greeting component.

---
+++ src/components/Greeting/Greeting.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Greeting.module.css'
+import photo from '../../assets/avatar.jpg'
+
+const Greeting = () => {
+    return (
+        <div className={classes.greeting}>
+            <div className={classes.text}>
+                <div>Hi 👋,</div>
+                <div>My name is</div>
+                <div className={classes.gradientText}>1mpossible-code</div>
+                <div>And I am a web developer</div>
+            </div>
+            <div>
+                <div className={classes.imageBorder}>
+                    <img src={photo} alt="" className={classes.image}/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Greeting;
