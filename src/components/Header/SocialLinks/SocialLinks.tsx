d7f406598e68f09a0065d1252acfe601e9609e65
Max <linme00p@gmail.com>
2022-06-14 15:40:56 +0200

    Make working SocialLinks.

--- src/components/Header/SocialLinks/SocialLinks.tsx
+++ src/components/Header/SocialLinks/SocialLinks.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import {GithubFilled, LinkedinFilled, TwitterCircleFilled} from "@ant-design/icons";
+import {GithubFilled, LinkedinFilled, TwitterCircleFilled} from '@ant-design/icons';
 import classes from './SocialLinks.module.css';
 
 const SocialLinks = () => {
+    const handleGithubClick = () => {
+        window.location.href = 'https://github.com/1mpossible-code/github-profile-page'
+    };
+
+    const handleTwitterClick = () => {
+        window.location.href = 'https://twitter.com/1mpossiblemax'
+    };
+
+    const handleLinkedInClick = () => {
+        window.location.href = 'https://www.linkedin.com/in/maksym-yemelianenko-622431183'
+    };
+
     return (
         <div className={classes.nav}>
-            <div className={classes.link}><GithubFilled/></div>
-            <div className={classes.link}><TwitterCircleFilled/></div>
-            <div className={classes.link}><LinkedinFilled style={{fontSize: '30px'}}/></div>
+            <div className={classes.link} onClick={handleGithubClick}><GithubFilled/></div>
+            <div className={classes.link} onClick={handleTwitterClick}><TwitterCircleFilled/></div>
+            <div className={classes.link} onClick={handleLinkedInClick}><LinkedinFilled/></div>
         </div>
     );
 };
