c94a56848526c4737326252cb543004e395d7cc9
Max <linme00p@gmail.com>
2022-06-14 17:44:54 +0200

    Create ProjectsPage.

--- src/pages/Projects/ProjectsPage.tsx
+++ src/pages/Projects/ProjectsPage.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import CategoryTitle from '../../components/common/CategoryTitle/CategoryTitle';
+import classes from './ProjectsPage.module.css';
+import Projects from '../../components/Projects/Projects';
+import ProjectList from '../../components/Projects/ProjectList/ProjectList';
 
 const ProjectsPage = () => {
     return (
-        <>
-            <h1>Projects</h1>
-        </>
+        <section className={classes.container}>
+            <CategoryTitle title={'Projects'}/>
+            <div className={classes.description}>
+                Things I’ve built so far
+            </div>
+            <ProjectList/>
+        </section>
     );
 };
 
