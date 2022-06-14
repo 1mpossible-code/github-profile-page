1b47a3a2145d5e0037094bc7c2aa59efe820e385
Max <linme00p@gmail.com>
2022-06-14 18:09:20 +0200

    Create smooth scrolling for Homepage.

--- src/components/Header/NavigationLinks/NavigationLinks.tsx
+++ src/components/Header/NavigationLinks/NavigationLinks.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from './NavigationLinks.module.css'
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
+import {Link as Scroll} from 'react-scroll'
 
 const NavigationLinks = () => {
+
+    const location = useLocation()
+    const [isScrolling, setIsScrolling] = useState(location.pathname == '/');
+
+    useEffect(() => {
+        if (location.pathname == '/') {
+            setIsScrolling(true)
+        } else {
+            setIsScrolling(false)
+        }
+    }, [location]);
+
     return (
         <nav className={classes.nav}>
-            <div className={classes.link}><Link className="link" to="/">Home</Link></div>
+            <div className={classes.link}>
+                {isScrolling ?
+                    <Scroll to={'header'} smooth={true}>Home</Scroll>
+                    :
+                    <Link className="link" to="/">Home</Link>
+                }
+
+            </div>
             <div className={classes.link}><Link className="link" to="/about">About</Link></div>
             <div className={classes.link}><Link className="link" to="/stack">Tech Stack</Link></div>
             <div className={classes.link}><Link className="link" to="/projects">Projects</Link></div>
