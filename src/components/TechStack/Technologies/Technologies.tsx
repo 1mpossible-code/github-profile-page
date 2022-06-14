f2cbee6ef19502828c34cec01db474059cdb9175
Max <linme00p@gmail.com>
2022-06-14 10:57:19 +0200

    Update Technologies list.

--- src/components/TechStack/Technologies/Technologies.tsx
+++ src/components/TechStack/Technologies/Technologies.tsx
@@ -9,12 +9,12 @@ import bootstrap from '../../../assets/logos/bootstrap.png'
 import jest from '../../../assets/logos/jest.png'
 import typescript from '../../../assets/logos/typescript.png'
 import git from '../../../assets/logos/git.png'
-import storybook from '../../../assets/logos/storybook.png'
+import tailwindcss from '../../../assets/logos/tailwindcss.png'
 import nextjs from '../../../assets/logos/nextjs.png'
 import github from '../../../assets/logos/github.png'
 import Technology from './Technology/Technology';
 
-const technologies = [html, css, js, react, redux, bootstrap, jest, typescript, git, storybook, nextjs, github]
+const technologies = [html, css, js, typescript, react, redux, bootstrap, tailwindcss, git, jest, nextjs, github]
 
 const Technologies = () => {
     return (
@@ -22,8 +22,8 @@ const Technologies = () => {
             {
                 technologies.map(
                     (image, idx) => (
-                        <div className={classes.technology}>
-                            <Technology src={image} key={idx}/>
+                        <div className={classes.technology} key={idx}>
+                            <Technology src={image}/>
                         </div>
                     )
                 )
