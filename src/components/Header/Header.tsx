ab0eabdd95b3c23bb7f2d41b40c3dbb1286518f0
Max <linme00p@gmail.com>
2022-06-20 15:40:14 +0200

    Create Header media.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Header.module.css';
 import SocialLinks from './SocialLinks/SocialLinks';
 import NavigationLinks from './NavigationLinks/NavigationLinks';
 import Logo from './Logo/Logo';
+import {CloseOutlined, MenuOutlined} from '@ant-design/icons';
+import classNames from 'classnames';
 
 const Header = () => {
+
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenu((prevState) => !prevState)
+    }
+
     return (
         <header className={classes.header} id="header">
             <Logo/>
-            <div className={classes.nav}>
+            <div onClick={toggleMobileMenu} className={classes.menu}>
+                {
+                    mobileMenu ? <CloseOutlined/> : <MenuOutlined/>
+                }
+            </div>
+            <div className={classNames(mobileMenu && classes.active, classes.nav)} onClick={toggleMobileMenu}>
                 <div className={classes.navLinks}>
                     <NavigationLinks/>
                 </div>
