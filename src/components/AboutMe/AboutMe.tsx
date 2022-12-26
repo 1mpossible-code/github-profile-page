2cdbf869ad50338388ffd02bb4e8ac3af544e042
Max <linme00p@gmail.com>
2022-12-26 14:55:42 -0500

    Update About Me section to use .env instead of hardcoded values.

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -6,13 +6,7 @@ const AboutMe = () => {
     return (
         <section className={classes.container}>
             <CategoryTitle title={'About Me'}/>
-            <div className={classes.description}>
-                Self-motivated, diverse, and fast-learning full-stack developer with 3 years experience in programming.
-                I have developed different projects on various levels starting with chatbots and parsers and ending with
-                APIs and UIs for SPA. Have a great experience in team-working and team-building. Currently, I am a
-                mentor for a group of people, whom I try to make programmers with a strong understanding of architecture
-                principles and clean coding.
-            </div>
+            <div className={classes.description}>{process.env.REACT_APP_ABOUT_ME}</div>
         </section>
     );
 };
